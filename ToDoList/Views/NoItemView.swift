868160707e3e05dde7544b119a960b9ad6f79dca
Asma <asma@Asmas-MBP.home>
2024-09-09 04:39:45 +0300

    first commit

---
+++ ToDoList/Views/NoItemView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemView.swift
+//  ToDoList
+//
+//  Created by Asma on 9/8/24.
+//
+
+import SwiftUI
+
+struct NoItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemView()
+}
